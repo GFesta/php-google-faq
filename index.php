b0c04f5b4d5523141d736180c4b38b58040c2aeb
Giuseppe <festagiuseppe89@gmail.com>
2021-04-26 15:24:37 +0200

    header / main completo

--- index.php
+++ index.php
@@ -82,3 +82,39 @@ $faq = [
     ]
 ]
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <title>php-google-faq</title>
+    </head>
+    <body>
+        <!-- Header -->
+    <header>
+        <div class="header-top">
+        <h2><img src="img/logo-google.png" alt="logo-google"> Privacy e termini</h2>
+        </div>
+        <div class="header-bottom">
+        <div class="header-bottom-left left">
+            <ul class="list-inline">
+            <li><a href="#">Introduzione</a></li>
+            <li><a href="#">Norme sulla privacy</a></li>
+            <li><a href="#">Termini di servizio</a></li>
+            <li><a href="#">Tecnologie</a></li>
+            <li><a class="active" href="#">Domande frequenti</a></li>
+            </ul>
+        </div>
+    </header>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main>
+        
+
+    </main>
+    <!-- /Main -->
+
+    </body>
+</html>
